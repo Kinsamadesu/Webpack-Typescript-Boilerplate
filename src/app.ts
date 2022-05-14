96afad2748fabf3feb74f9d086c32296caee8266
Kinsamadesu <mimisteus@gmail.com>
2022-05-14 17:52:11 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import './Scss/app.scss'
+
+console.log('app entry point')
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: '.' }],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
